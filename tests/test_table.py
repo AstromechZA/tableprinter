8f01d1087231ae46312c44ac59c07ebaffdb10f6
Ben Meier <benmeier@fastmail.com>
2015-01-08 00:01:32 +0200

    100% ! yay!

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,4 +1,5 @@
 import pytest
+from mock import patch
 from pytabular.table import Table
 
 def test_construct():
@@ -29,3 +30,32 @@ def test_sort_no_data():
 def test_repr():
     t = Table([['hello', 'world']], title='Test Table')
     assert repr(t) == "Table(title='Test Table', shape=(1, 2), column_names=None)"
+
+def test_tables():
+    data = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print Table(data, title='A longer title with words.')
+    print Table(data, title='compact', compact=True)
+    print Table(data, title='reversed', sort_reverse=True)
+
+    data = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7]
+    ]
+
+    print Table(data, column_names=['c1', 'c2', 'c3'])
+    print Table(data, column_names=['c1', 'c2'])
+
+    with patch('pytabular.utils.get_terminal_width') as tw_mock:
+        tw_mock.return_value = 30
+
+        data = [
+            ['some fairly long string', 'another longish string']
+        ]
+
+        print Table(data, column_names=['c1', 'c2'])
